30361633573bdc65ba8194098a361c42cee0653f
dboatengg <dicksonboateng67@gmail.com>
2022-07-28 15:59:09 +0000

    Implement mobile responsiveness

---
+++ main.js
@@ -0,0 +1,21 @@
+// let radio = document.querySelectorAll(".opt");
+// let formEle = document.querySelector(".form");
+
+// Array.from(radio).forEach(function (radioItem) {
+//   if (radioItem.checked === true) {
+//     formEle.style.backgroundColor = "red";
+//     console.log("hello");
+// formEle.style.display = "none";
+//   }
+// formEle.style.display = "none";
+//   formEle.style.backgroundColor = "blue";
+//   console.log("hi");
+// });
+
+// Array.from(radio).forEach(function (ele) {
+//   if (ele.checked === true) {
+//     console.log("hello");
+//   }
+//   console.log("wow");
+// });
+// showForm();
